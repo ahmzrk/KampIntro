e74d0fb1a637f45788aaf6f6399b8405b412641d
Redoes <redoes123@hotmail.com>
2024-08-05 15:58:25 +0300

    Proje dosyası ekle.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Ahmet");
+            Console.WriteLine(isimler.Count);
+            isimler.Add("İsmet");
+            Console.WriteLine(isimler.Count);
+
+            foreach (var item in isimler.Items) 
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+        
+    }
+}
